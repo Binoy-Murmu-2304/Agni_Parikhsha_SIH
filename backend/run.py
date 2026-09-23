b6e39190bcc8e0d6e964b96a93b7d617b3a71bab
Biinoy Murmu <binoy.murmu.2311@gmail.com>
2026-09-23 18:47:21 +0530

    Clean up stray files for clean-room start

--- backend/run.py
+++
@@ -1,6 +0,0 @@
-import uvicorn
-import os
-
-if __name__ == "__main__":
-    os.environ["PYTHONPATH"] = os.path.abspath(os.path.dirname(__file__))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
